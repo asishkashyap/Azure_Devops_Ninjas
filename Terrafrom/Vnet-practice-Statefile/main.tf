1c33745d1880f318092144f511126802e62dcb11
Asish Kashyap <129969015+asishkashyap@users.noreply.github.com>
2024-12-07 09:41:42 +0530

    Update main.tf

--- Terrafrom/Vnet-practice-Statefile/main.tf
+++ Terrafrom/Vnet-practice-Statefile/main.tf
@@ -21,29 +21,6 @@ resource "azurerm_virtual_network" "vnet-block" {
 
 
 
-resource "azurerm_virtual_network" "vnet-block1" {
-  name                = "Rasmalai-vnet1"
-  location            = "Central India"
-  resource_group_name = "ShahiRasmalai"
-  address_space       = ["10.0.0.0/16"]
-
-
-  subnet {
-    name           = "malai12"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "malai21"
-    address_prefix = "10.0.2.0/24"
-  }
-
-
-}
-
-
-
-
 
 
 
